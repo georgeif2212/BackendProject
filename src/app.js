78523087c47216e431022c8eb8db54bfa1c718af
georgeif2212 <jinfante2212@gmail.com>
2023-10-14 15:54:13 -0600

    Tarea terminada con params y query funcionando

--- src/app.js
+++ src/app.js
@@ -17,6 +17,20 @@ app.get("/products", async (req, res) => {
   }
 });
 
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const products = await productManager.getProducts();
+  const product = products.find((product) => {
+    return product.id === parseInt(productId);
+  });
+
+  if (!product) {
+    res.json({ error: `Usuario con id ${productId} no encontrado.`})
+  } else {
+    res.json(product);
+  }
+});
+
 app.listen(8080, () => {
   console.log("HTTP Server listening from 8080 port");
 });

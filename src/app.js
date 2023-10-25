d0856e4a1713fad0e41fee00ddd49a0180511600
georgeif2212 <jinfante2212@gmail.com>
2023-10-24 22:41:12 -0600

    Configuración de modularización para router de productos app.js terminada

--- src/app.js
+++ src/app.js
@@ -1,36 +1,18 @@
-const ProductManager = require("./ProductManager.js");
 const express = require("express");
+const path = require("path");
+
+const productsRouter = require("./routers/products.router");
+
 const app = express();
+const PORT = 8080;
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const productManager = new ProductManager("./Products.json");
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/products", async (req, res) => {
-  const { query } = req;
-  const { limit } = query;
-  const products = await productManager.getProducts();
-  if (!limit) {
-    res.json(products);
-  } else {
-    const limitedProducts = products.slice(0, parseInt(limit));
-    res.json(limitedProducts);
-  }
-});
+app.use("/api", productsRouter);
 
-app.get("/products/:productId", async (req, res) => {
-  const { productId } = req.params;
-  const products = await productManager.getProducts();
-  const product = products.find((product) => {
-    return product.id === parseInt(productId);
-  });
-
-  if (!product) {
-    res.json({ error: `Usuario con id ${productId} no encontrado.`})
-  } else {
-    res.json(product);
-  }
-});
-
-app.listen(8080, () => {
-  console.log("HTTP Server listening from 8080 port");
+app.listen(PORT, () => {
+  console.log(`HTTP Server listening from ${PORT} port`);
 });

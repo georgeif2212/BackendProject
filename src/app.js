d41ef75e0aa3de6ce259ab5150cb0c5546bbb0b9
georgeif2212 <jinfante2212@gmail.com>
2023-10-25 19:21:39 -0600

    Endpoints terminados para los carts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const productsRouter = require("./routers/products.router");
+const cartsRouter = require("./routers/carts.router");
 
 const app = express();
 const PORT = 8080;
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/api", productsRouter);
+app.use("/api", productsRouter, cartsRouter);
 
 app.listen(PORT, () => {
   console.log(`HTTP Server listening from ${PORT} port`);
---
+++ src/routers/carts.router.js
@@ -0,0 +1,102 @@
+const { Router } = require("express");
+const { v4: uuidV4 } = require("uuid");
+const router = Router();
+
+const carts = [
+  {
+    id: "8e1e9f2c-41e6-48b8-8fc5-33a9c0e7da1f",
+    products: [
+      {
+        id: 1,
+        title: "Esto es para actualizar el producto 1 por su id",
+        description: "Este es un producto actualizado 1",
+        quantity: 3,
+      },
+      {
+        id: 5,
+        title: "Producto prueba 5",
+        description: "Este es un producto prueba 5",
+        price: 543,
+        quantity: 12,
+      },
+    ],
+  },
+  {
+    id: "3df64235-0e45-44c8-a082-378677ef7f96",
+    products: [
+      {
+        id: 10,
+        title: "Producto prueba 10",
+        description: "Este es un producto prueba 10",
+        price: 654,
+        quantity: 5,
+      },
+      {
+        id: "faddee08-eb7d-426d-abe5-7ecb3c83aaf2",
+        title: "Producto prueba POST",
+        description: "Este es un producto prueba POST",
+        price: 2924.45,
+        quantity: 9,
+      },
+    ],
+  },
+];
+
+router.get("/carts", (req, res) => {
+  res.status(200).json(carts);
+});
+
+router.post("/carts", (req, res) => {
+  const { body } = req;
+  const newCart = {
+    id: uuidV4(),
+    products: [body],
+  };
+  carts.push(newCart);
+  res.status(201).json(newCart);
+});
+
+router.get("/carts/:cartId", (req, res) => {
+  const { cartId } = req.params;
+  const cart = carts.find((cart) => cart.id === cartId);
+
+  if (!cart) return res.status(404).json({ error: "Cart not found." });
+
+  const cartProducts = cart.products;
+  res.status(200).json({
+    id: cart.id,
+    products: cartProducts,
+  });
+});
+
+router.post("/carts/:cartId/products/:productId", (req, res) => {
+  const { cartId, productId } = req.params;
+  const { body } = req; //{quantity: 3}
+
+  // * Busca al carrito que corresponda al cartID por params
+  const cart = carts.find((cart) => cart.id == cartId);
+  if (!cart) return res.status(404).json({ error: "Cart not found." });
+
+  // * Busca al producto dentro del carrito que corresponda al productID por params
+  const cartProducts = cart.products;
+
+  const product = cartProducts.find((product) => product.id == productId);
+  // * Si no hay producto añadir uno nuevo
+  if (!product) {
+    // * Crear el nuevo producto que se añadirá al carrito
+    const newProduct = {
+      id: productId,
+      quantity: body.quantity,
+    };
+    cartProducts.push(newProduct);
+  } else {
+    product.quantity += body.quantity;
+  }
+
+  res.status(200).json({
+    id: cart.id,
+    products: cartProducts,
+  });
+});
+
+module.exports = router;

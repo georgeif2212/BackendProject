1d2b815a61c6d85564ed037f638d67a4f452515d
georgeif2212 <jinfante2212@gmail.com>
2023-11-26 21:27:24 -0600

    news styles

--- public/js/index.js
+++ public/js/index.js
@@ -51,33 +51,39 @@ socket.on("update-list-products", ({ products }) => {
 
   products.forEach((product) => {
     const article = document.createElement("article");
-    article.style.width = "30%";
-
-    const idParagraph = document.createElement("p");
-    idParagraph.innerHTML = `<strong>ID</strong>: ${product._id}`;
-    article.appendChild(idParagraph);
-
-    const nameParagraph = document.createElement("p");
-    nameParagraph.innerHTML = `<strong>Nombre</strong>: ${product.title}`;
-    article.appendChild(nameParagraph);
-
-    const priceParagraph = document.createElement("p");
-    priceParagraph.innerHTML = `<strong>Precio</strong>: ${product.price}`;
-    article.appendChild(priceParagraph);
-
-    const descriptionParagraph = document.createElement("p");
-    descriptionParagraph.innerHTML = `<strong>Descripción</strong>: ${product.description}`;
-    article.appendChild(descriptionParagraph);
-
-    const stockParagraph = document.createElement("p");
-    stockParagraph.innerHTML = `<strong>Stock</strong>: ${product.stock}`;
-    article.appendChild(stockParagraph);
+    article.classList.add("card", "product-real-time");
 
     const thumbnailImage = document.createElement("img");
-    thumbnailImage.style.height = "30%";
+    thumbnailImage.classList.add("card-img-top","product-real-time__img");
     thumbnailImage.src = product.thumbnail;
     thumbnailImage.alt = "";
     article.appendChild(thumbnailImage);
+
+    const div = document.createElement("div");
+    div.classList.add("card-body");
+    article.append(div);
+
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.innerHTML = `${product.title}`;
+    div.appendChild(title);
+
+    let p = document.createElement("p");
+    p.classList.add("card-text");
+    p.innerHTML = `<strong>ID</strong>: ${product._id}`;
+    div.appendChild(p);
+
+    p = document.createElement("p");
+    p.innerHTML = `<strong>Precio</strong>: ${product.price}`;
+    div.appendChild(p);
+
+    p = document.createElement("p");
+    p.innerHTML = `<strong>Descripción</strong>: ${product.description}`;
+    div.appendChild(p);
+
+    p = document.createElement("p");
+    p.innerHTML = `<strong>Stock</strong>: ${product.stock}`;
+    div.appendChild(p);
 
     listProducts.appendChild(article);
   });

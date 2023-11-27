349413872e12832b9e7386e2d88a967ee5ed41b7
georgeif2212 <jinfante2212@gmail.com>
2023-11-26 23:06:19 -0600

    Products view paginated

--- src/routers/views/views.router.js
+++ src/routers/views/views.router.js
@@ -2,6 +2,7 @@ import ProductsManager from "../../dao/Products.manager.js";
 import { __dirname } from "../../utils.js";
 import { Router } from "express";
 import { emit } from "../../socket.js";
+import { buildResponsePaginated } from "../../utils.js";
 
 const router = Router();
 // * Ruta con path porque sin ella no me daba
@@ -13,21 +14,23 @@ const router = Router();
 
 // ! ENDPOINTS FOR PRODUCTS
 router.get("/products", async (req, res) => {
-  const { query } = req;
-  const { limit } = query;
-  if (!limit) {
-    const products = await ProductsManager.get();
-    res.status(200).render("home", {
-      title: "Products 🧴",
-      products: products.map((product) => product.toJSON()),
-    });
-  } else {
-    const products = await ProductsManager.get(parseInt(limit));
-    res.status(200).render("home", {
-      title: "Limited Products 🧴",
-      products: products.map((product) => product.toJSON()),
-    });
+  const { limit = 10, page = 1, sort, search } = req.query;
+
+  const criteria = {};
+  const options = { limit, page };
+  if (sort) {
+    options.sort = { price: sort };
   }
+  if (search) {
+    criteria.title = search;
+  }
+  const baseUrl = "http://localhost:8080/views";
+  const result = await ProductsManager.get(criteria, options);
+  const data = buildResponsePaginated({ ...result, sort, search }, baseUrl);
+  res.status(200).render("home", {
+    title: "Products 🧴",
+    ...data,
+  });
 });
 
 // ! ENDPOINTS FOR REALTIMEPRODUCTS

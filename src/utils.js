4f148a12421f98482c587c11931418769cfe1b85
georgeif2212 <jinfante2212@gmail.com>
2023-11-27 21:40:37 -0600

    Build response paginated for carts

--- src/utils.js
+++ src/utils.js
@@ -34,3 +34,33 @@ export const buildResponsePaginated = (data, baseUrl = URL_BASE) => {
       : null,
   };
 };
+
+export const URL_BASE_CARTS = "http://localhost:8080/api/carts";
+export const buildResponsePaginatedCarts = (data, baseUrl = URL_BASE_CARTS) => {
+  return {
+    //status:success/error
+    status: "success",
+    //payload: Resultado de los productos solicitados
+    payload: data.docs.map((doc) => doc.toJSON()),
+    //totalPages: Total de páginas
+    totalPages: data.totalPages,
+    //prevPage: Página anterior
+    prevPage: data.prevPage,
+    //nextPage: Página siguiente
+    nextPage: data.nextPage,
+    //page: Página actual
+    page: data.page,
+    //hasPrevPage: Indicador para saber si la página previa existe
+    hasPrevPage: data.hasPrevPage,
+    //hasNextPage: Indicador para saber si la página siguiente existe.
+    hasNextPage: data.hasNextPage,
+    //prevLink: Link directo a la página previa (null si hasPrevPage=false)
+    prevLink: data.hasPrevPage
+      ? `${baseUrl}?limit=${data.limit}&page=${data.prevPage}`
+      : null,
+    //nextLink: Link directo a la página siguiente (null si hasNextPage=false)
+    nextLink: data.hasNextPage
+      ? `${baseUrl}?limit=${data.limit}&page=${data.nextPage}`
+      : null,
+  };
+};

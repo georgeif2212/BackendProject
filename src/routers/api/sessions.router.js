22d3eea5af8ba903a0358f8bcc799623b000ae52
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 17:23:39 -0600

    Login route ended

--- src/routers/api/sessions.router.js
+++ src/routers/api/sessions.router.js
@@ -3,7 +3,27 @@ import UserManager from "../../dao/User.manager.js";
 
 const router = Router();
 
+router.post("/sessions/login", async (req, res) => {
+  try {
+    const { body } = req;
+    const user = await UserManager.login(body);
 
+    const { first_name, last_name, age, role, email } = user;
+    req.session.user = {
+      first_name,
+      last_name,
+      email,
+      age,
+      role,
+    };
+    res.redirect("/views/profile");
+  } catch (error) {
+    res.status(400).render("error", {
+      title: "Errores",
+      messageError: error.message,
+    });
+  }
+});
 
 router.post("/sessions/register", async (req, res) => {
   try {

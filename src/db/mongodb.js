ff21b1684ef95646eaf52899e17fceb47e67754a
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 15:22:32 -0600

    Exportation of URI Const

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
+export const URI =
+  "mongodb+srv://georgeif2212:EJldzR321JjOxY1u@cluster0.b9p1i7g.mongodb.net/ecommerce?retryWrites=true&w=majority";
+
 export const initDB = async () => {
   try {
-    const URI =
-      "mongodb+srv://georgeif2212:EJldzR321JjOxY1u@cluster0.b9p1i7g.mongodb.net/ecommerce?retryWrites=true&w=majority";
     await mongoose.connect(URI);
     console.log("Database connected 🚀");
   } catch (error) {

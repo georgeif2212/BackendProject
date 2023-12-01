a11209d00fa9ce58a051bb60b70d036146837df7
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 17:37:05 -0600

    Logout and me route added

--- src/routers/api/sessions.router.js
+++ src/routers/api/sessions.router.js
@@ -38,4 +38,23 @@ router.post("/sessions/register", async (req, res) => {
   }
 });
 
+router.get("/sessions/me", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ message: "No estas autenticado." });
+  }
+  res.status(200).json(req.session.user);
+});
+
+router.get("/session/logout", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      return res.render("error", {
+        title: "Hello People 🖐️",
+        messageError: error.message,
+      });
+    }
+    res.redirect("/views/login");
+  });
+});
+
 export default router;

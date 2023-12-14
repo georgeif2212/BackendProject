94f653f2290b14ad2c2d33a0b34775e77a3de66b
georgeif2212 <jinfante2212@gmail.com>
2023-12-13 22:07:17 -0600

    Model modified to accept providers

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -1,12 +1,17 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: 'user' },
-  age: { type: Number, required: false },
-}, { timestamps: true });
+const userSchema = new Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    provider: { type: String },
+    providerId: { type: String },
+    role: { type: String, default: "user" },
+    age: { type: Number, required: false },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);

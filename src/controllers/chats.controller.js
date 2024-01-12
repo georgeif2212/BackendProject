60b42d34a435cbd7e2aba524cde2f20ffa5966d9
georgeif2212 <jinfante2212@gmail.com>
2024-01-11 23:29:12 -0600

    perf: chats controller adapted to work with the exceptions

--- src/controllers/chats.controller.js
+++ src/controllers/chats.controller.js
@@ -1,6 +1,7 @@
+import { InvalidDataException, NotFoundException } from "../utils.js";
 import MessageModel from "./models/message.model.js";
 
-export default class ChatsManager {
+export default class ChatsController {
   static get(limit) {
     if (!limit) return MessageModel.find();
     return MessageModel.find().limit(limit);
@@ -8,28 +9,28 @@ export default class ChatsManager {
 
   static async getById(sid) {
     const cart = await MessageModel.findById(sid);
-    if (!cart) {
-      throw new Error(`Cart with ${sid} not found`);
-    }
+    if (!cart) throw new NotFoundException(`Cart with ${sid} not found`);
+
     return cart;
   }
 
   static create(data) {
     const { user, message } = data;
-
+    if (!user || !message)
+      throw new InvalidDataException("Usuario y mensaje requeridos");
     return MessageModel.create(data);
   }
 
   static async updateById(sid, data) {
     const product = await ProductsManager.getById(sid);
-    if (!product) throw new Error(`Product with ${sid} not found`);
+    if (!product) throw new NotFoundException(`Product with ${sid} not found`);
     await MessageModel.updateOne({ _id: sid }, { $set: { products: data } });
     console.log(`Producto actualizado correctamente (${sid}) 😁.`);
   }
 
   static async deleteById(sid) {
     const product = await ProductsManager.getById(sid);
-    if (!product) throw new Error(`Product with ${sid} not found`);
+    if (!product) throw new NotFoundException(`Product with ${sid} not found`);
 
     await MessageModel.deleteOne({ _id: sid });
     console.log(`Producto eliminado correctamente (${sid}) 🤔.`);

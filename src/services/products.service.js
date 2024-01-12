1bf2647aa5599eec878d253c57ca0bdf9b08497f
georgeif2212 <jinfante2212@gmail.com>
2024-01-12 13:13:38 -0600

    feat: products service able to communicate between service controller and serviceDao

---
+++ src/services/products.service.js
@@ -0,0 +1,29 @@
+import ProductDao from "../dao/product.dao.js";
+
+export default class ProductsService {
+  static getPaginate(criteria, options) {
+    return ProductDao.getPaginate(criteria, options);
+  }
+
+  static create(data) {
+    return ProductDao.create(data);
+  }
+
+  static async getById(id) {
+    const result = await ProductDao.getFilter({ _id: id });
+    return result[0];
+  }
+
+  static updateById(id, data) {
+    return ProductDao.updateById(id, data);
+  }
+
+  static deleteById(id) {
+    return ProductDao.deleteById(id);
+  }
+
+  static async getByCode(code) {
+    const result = await ProductDao.getFilter({ code: code });
+    return result[0];
+  }
+}

968307425acd82c0f4db4a423ec8061ecc11141e
georgeif2212 <jinfante2212@gmail.com>
2024-01-11 20:49:40 -0600

    feat: New exceptions added

--- src/utils.js
+++ src/utils.js
@@ -73,3 +73,34 @@ export const createHash = (password) =>
 
 export const isValidPassword = (password, user) =>
   bcrypt.compareSync(password, user.password);
+
+export class Exception extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class InvalidDataException extends Exception {
+  constructor(message) {
+    super(message, 400);
+  }
+}
+
+export class NotFoundException extends Exception {
+  constructor(message) {
+    super(message, 404);
+  }
+}
+
+export class UnauthorizedException extends Exception {
+  constructor(message) {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenException extends Exception {
+  constructor(message) {
+    super(message, 403);
+  }
+}

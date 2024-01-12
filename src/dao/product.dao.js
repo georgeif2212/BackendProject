da2919fae867e4e5d3376c05f03d22c06335cd78
georgeif2212 <jinfante2212@gmail.com>
2024-01-12 13:13:07 -0600

    feat: product dao able to communicate to dao service

---
+++ src/dao/product.dao.js
@@ -0,0 +1,23 @@
+import ProductModel from "./models/product.model.js";
+
+export default class ProductDaoMongoDB {
+  static getPaginate(criteria, options) {
+    return ProductModel.paginate(criteria, options);
+  }
+
+  static getFilter(criteria = {}) {
+    return ProductModel.find(criteria);
+  }
+
+  static create(data) {
+    return ProductModel.create(data);
+  }
+
+  static updateById(uid, data) {
+    return ProductModel.updateOne({ _id: uid }, { $set: data });
+  }
+
+  static deleteById(uid) {
+    return ProductModel.deleteOne({ _id: uid });
+  }
+}

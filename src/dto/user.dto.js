8ab211b8e2135822eb7d93efd712aac1c4a1b4ff
georgeif2212 <jinfante2212@gmail.com>
2024-01-20 13:59:39 -0600

    fix: UserDTO was using id instead _id

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -1,6 +1,6 @@
 export default class UserDTO {
   constructor(user) {
-    this.id = user._id || user.id;
+    this._id = user._id || user.id;
     this.first_name = user.first_name;
     this.last_name = user.last_name;
     this.email = user.email;

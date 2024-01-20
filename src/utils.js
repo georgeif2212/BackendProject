6a25b4fdcc78934a98a01f414d4468646adbae19
georgeif2212 <jinfante2212@gmail.com>
2024-01-20 17:03:03 -0600

    feat: middleware for authentication and roles added

--- src/utils.js
+++ src/utils.js
@@ -104,3 +104,29 @@ export class ForbiddenException extends Exception {
     super(message, 403);
   }
 }
+
+export const authMiddleware = (strategy) => (req, res, next) => {
+  passport.authenticate(strategy, function (error, payload, info) {
+    if (error) {
+      return next(error);
+    }
+    if (!payload) {
+      return res
+        .status(401)
+        .json({ message: info.message ? info.message : info.toString() });
+    }
+    req.user = payload;
+    next();
+  })(req, res, next);
+};
+
+export const authRolesMiddleware = (role) => (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const { role: userRole } = req.user;
+  if (userRole !== role) {
+    return res.status(403).json({ message: "No permissions" });
+  }
+  next();
+};

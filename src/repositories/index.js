e324b8e46648bd23ad3674a755ab221399940a73
georgeif2212 <jinfante2212@gmail.com>
2024-01-19 21:09:35 -0600

    feat: User repository file added

--- src/repositories/index.js
+++
@@ -1,5 +0,0 @@
-import { contactDao } from '../dao/factory.js';
-
-import ContactsRepository from './contacts.repository.js';
-
-export const contactsRepository = new ContactsRepository(contactDao);
---
+++ src/repositories/users.repository.js
@@ -0,0 +1,38 @@
+import UserDto from "../dto/user.dto.js";
+import UserDaoMongoDB from "../dao/user.dao.js";
+
+export default class UserRepository {
+  static async getPaginate(filter = {}, opts = {}) {
+    const users = await UserDaoMongoDB.getPaginate(filter, opts);
+    return users.map((user) => new UserDto(user));
+  }
+
+  async getById(uid) {
+    let user = await UserDaoMongoDB.getFilter({ _id: uid });
+    if (user) {
+      user = new UserDto(user);
+    }
+    return user;
+  }
+
+  static async create(data) {
+    const user = await UserDaoMongoDB.create(data);
+    return new UserDto(user);
+  }
+
+  static updateById(uid, data) {
+    return UserDaoMongoDB.updateById(uid, data);
+  }
+
+  static deleteById(uid) {
+    return UserDaoMongoDB.deleteById(uid);
+  }
+
+  async getByEmail(email) {
+    let user = await UserDaoMongoDB.getFilter({ email: email });
+    if (user) {
+      user = new UserDto(user);
+    }
+    return user;
+  }
+}

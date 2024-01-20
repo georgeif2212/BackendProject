44046f4cb8b8d094ba09f7c43e1e847632bdcc55
georgeif2212 <jinfante2212@gmail.com>
2024-01-20 16:07:23 -0600

    fix: Carts service adapted to work with repository

--- src/services/carts.service.js
+++ src/services/carts.service.js
@@ -1,29 +1,23 @@
-import CartDao from "../dao/cart.dao.js";
+import CartRepository from "../repositories/carts.repository.js";
 
 export default class CartsService {
   static getPaginate(criteria, options) {
-    return CartDao.getPaginate(criteria, options);
+    return CartRepository.getPaginate(criteria, options);
   }
 
   static create(data) {
-    return CartDao.create(data);
+    return CartRepository.create(data);
   }
 
-  static async getById(id) {
-    const result = await CartDao.getFilter({ _id: id });
-    return result[0];
+  static getById(id) {
+    return CartRepository.getFilter({ _id: id });
   }
 
   static updateById(id, data) {
-    return CartDao.updateById(id, data);
+    return CartRepository.updateById(id, data);
   }
 
   static deleteById(id) {
-    return CartDao.deleteById(id);
-  }
-
-  static async getByEmail(email) {
-    const result = await CartDao.getFilter({ email: email });
-    return result[0];
+    return CartRepository.deleteById(id);
   }
 }

1df51c9826db0fd110c0f5d2ba2aac9fac4d8f0b
georgeif2212 <jinfante2212@gmail.com>
2024-01-21 13:02:47 -0600

    fix: passport import was missing

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import path from "path";
 import url from "url";
 import bcrypt from "bcrypt";
+import passport from "passport";
 
 const __filename = url.fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);

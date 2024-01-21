f9126f905b4e92b48666fce8e989a053881cee8a
georgeif2212 <jinfante2212@gmail.com>
2024-01-21 17:43:38 -0600

    fix: session secret no needed

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,6 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   mongodbUri: process.env.MONGODB_URI,
-  sessionSecret: process.env.SESSION_SECRET,
   persistence: process.env.PERSISTENCE || "memory",
   jwtSecret: process.env.JWT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,

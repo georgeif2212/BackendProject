f97f161df15bf65ab70f2debf5b2799f7a825e99
georgeif2212 <jinfante2212@gmail.com>
2024-01-21 13:34:53 -0600

    feat: jwt secret added

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,6 @@ export default {
   port: process.env.PORT,
   mongodbUri: process.env.MONGODB_URI,
   sessionSecret: process.env.SESSION_SECRET,
-  persistence: process.env.PERSISTENCE || 'memory',
+  persistence: process.env.PERSISTENCE || "memory",
+  jwtSecret: process.env.JWT_SECRET,
 };

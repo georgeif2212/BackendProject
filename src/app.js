c4f31ddbdb50970678542c3c27c0a4ce278f6a7e
georgeif2212 <jinfante2212@gmail.com>
2024-01-21 13:39:13 -0600

    fix: app working with jwt cookie parser instead sessions

--- src/app.js
+++ src/app.js
@@ -2,11 +2,8 @@ import express from "express";
 import handlebars from "express-handlebars";
 import path from "path";
 import { Exception, __dirname } from "./utils.js";
-import { URI } from "./db/mongodb.js";
-import sessions from "express-session";
-import MongoStore from "connect-mongo";
 import passport from "passport";
-import config from "./config/config.js";
+import cookieParser from "cookie-parser";
 
 import productsRouter from "./routers/api/products.router.js";
 import cartsRouter from "./routers/api/carts.router.js";
@@ -17,21 +14,7 @@ import { init as initPassport } from "./config/passport.config.js";
 
 const app = express();
 
-const SESSION_SECRET = config.sessionSecret;
-
-app.use(
-  sessions({
-    store: MongoStore.create({
-      mongoUrl: URI,
-      mongoOptions: {},
-      ttl: 60 * 30,
-    }),
-    secret: SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));

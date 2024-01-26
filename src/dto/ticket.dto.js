c3bdc678ced0367a70642abd339a68fbc0211aba
georgeif2212 <jinfante2212@gmail.com>
2024-01-26 15:48:43 -0600

    fix: purchase_datetime data was missing

--- src/dto/ticket.dto.js
+++ src/dto/ticket.dto.js
@@ -4,5 +4,6 @@ export default class TicketDTO {
     this.code = ticket.code;
     this.amount = ticket.amount;
     this.purchaser = ticket.purchaser;
+    this.purchase_datetime=ticket.createdAt;
   }
 }

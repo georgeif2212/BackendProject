e75f0270df076fc5f66053dbc13247054b96fa21
georgeif2212 <jinfante2212@gmail.com>
2024-01-26 17:46:15 -0600

    feat: the errors are throwed by custom error

--- src/controllers/chats.controller.js
+++ src/controllers/chats.controller.js
@@ -1,5 +1,9 @@
-import { InvalidDataException, NotFoundException } from "../utils.js";
 import ChatsService from "../services/chats.service.js";
+import { CustomError } from "../utils/CustomError.js";
+import {
+  generatorMessageError,
+  generatorMessageIdError,
+} from "../utils/CauseMessageError.js";
 
 export default class ChatsController {
   static get(limit) {
@@ -8,30 +12,33 @@ export default class ChatsController {
 
   static async getById(cid) {
     const message = await ChatsService.getById(cid);
-    if (!message) throw new NotFoundException(`Cart with ${cid} not found`);
 
+    if (!message) {
+      CustomError.create({
+        name: "Message not found",
+        cause: generatorMessageIdError(cid),
+        message: `Message with ${cid} not found`,
+        code: EnumsError.NOT_FOUND_ERROR,
+      });
+    }
     return message;
   }
 
   static create(data) {
     const { user, message } = data;
     if (!user || !message)
-      throw new InvalidDataException("Usuario y mensaje requeridos");
+      CustomError.create({
+        name: "Invalid data message",
+        cause: generatorMessageError(data),
+        message: `An error occurred while creating a message`,
+        code: EnumsError.BAD_REQUEST_ERROR,
+      });
     return ChatsService.create(data);
   }
 
-  // static async updateById(cid, data) {
-  //   const message = await ChatsController.getById(cid);
-  //   if (!message) throw new NotFoundException(`Message with ${cid} not found`);
-  //   await ChatsService.updateOne({ _id: cid }, { $set: { products: data } });
-  //   console.log(`Producto actualizado correctamente (${cid}) 😁.`);
-  // }
-
   static async deleteById(cid) {
     const message = await ChatsController.getById(cid);
-    if (!message) throw new NotFoundException(`Message with ${cid} not found`);
-
-    await ChatsService.deleteById(cid);
+    await ChatsService.deleteById(message._id);
     console.log(`Message eliminado correctamente (${cid}) 🤔.`);
   }
 }

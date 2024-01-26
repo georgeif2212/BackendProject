0579be9ca94b471997abd7b4f2dbdc8458309074
georgeif2212 <jinfante2212@gmail.com>
2024-01-26 16:00:07 -0600

    feat: new router for mocking products

---
+++ src/routers/api/mocks.router.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+// ! Muestra todos los carritos
+router.get("/mockingproducts", async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;

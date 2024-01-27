bee4e7d08f4dd649f3d13472facfc9277091cba9
georgeif2212 <jinfante2212@gmail.com>
2024-01-26 18:05:34 -0600

    feat: the errors are throwed by custom error

--- src/controllers/tickets.controller.js
+++ src/controllers/tickets.controller.js
@@ -1,6 +1,9 @@
-import { InvalidDataException, NotFoundException } from "../utils.js";
 import TicketsService from "../services/tickets.service.js";
 import { v4 as uuidv4 } from "uuid";
+import {
+  generatorTicketError,
+  generatorTicketIdError,
+} from "../utils/CauseMessageError.js";
 export default class TicketsController {
   static get(criteria, options) {
     return TicketsService.getPaginate(criteria, options);
@@ -13,17 +16,26 @@ export default class TicketsController {
   static async getById(tid) {
     const ticket = await TicketsService.getById(tid);
     if (!ticket) {
-      throw new NotFoundException(`Ticket with ${tid} not found`);
+      CustomError.create({
+        name: "Ticket not found",
+        cause: generatorTicketIdError(tid),
+        message: `Ticket with ${cid} not found`,
+        code: EnumsError.NOT_FOUND_ERROR,
+      });
     }
     return ticket;
   }
 
   static create(data) {
     const { email, availableProducts } = data;
-    if (!email || availableProducts.length == 0)
-      throw new InvalidDataException(
-        "There must be a buyer and products in the cart"
-      );
+    if (!email || availableProducts.length == 0) {
+      CustomError.create({
+        name: "Invalid ticket data",
+        cause: generatorTicketError(data),
+        message: `"There must be an email and available products"`,
+        code: EnumsError.BAD_REQUEST_ERROR,
+      });
+    }
     const amount = availableProducts.reduce((accumulator, element) => {
       return accumulator + element.product.price * element.quantity;
     }, 0);
@@ -39,17 +51,13 @@ export default class TicketsController {
 
   static async updateById(tid, data) {
     const ticket = await TicketsController.getById(tid);
-    if (!ticket) throw new NotFoundException(`ticket with ${tid} not found`);
-
-    await TicketsService.updateById(tid, data);
+    await TicketsService.updateById(ticket._id, data);
     console.log(`ticket actualizado correctamente (${tid}) 😁.`);
   }
 
   static async deleteById(tid) {
     const ticket = await TicketsController.getById(tid);
-    if (!ticket) throw new NotFoundException(`ticket with ${tid} not found`);
-
-    await TicketsService.deleteById(tid);
+    await TicketsService.deleteById(ticket._id);
     console.log(`ticket eliminado correctamente (${tid}) 🤔.`);
   }
 

c1228453ae241acbbe6151bf24a2d28c74013d4e
georgeif2212 <jinfante2212@gmail.com>
2024-02-21 22:40:23 -0600

    feat: middleware to validate recover password token implemented and auth with roles middleware moved to this directory

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,49 @@
+import { validateToken } from "../utils/utils.js";
+
+export const tokenRecoverPassword = async (req, res, next) => {
+  const token = req.query.token;
+  const payload = await validateToken(token);
+  if (!payload) {
+    return res.status(401).json({
+      message: "JWT Token is invalid, do you want to recover again?",
+      recover: "http://localhost:8080/views/email-recover-password",
+      login: "http://localhost:8080/views/login",
+    });
+  }
+  if (payload.type != "recoverPassword") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  req.user = payload;
+  next();
+};
+
+export const authMiddleware = (strategy) => (req, res, next) => {
+  passport.authenticate(strategy, function (error, payload, info) {
+    if (error) {
+      return next(error);
+    }
+    if (!payload) {
+      return res.status(401).json({
+        message: info.message ? info.message : info.toString(),
+        login: "http://localhost:8080/views/login",
+      });
+    }
+    req.user = payload;
+    next();
+  })(req, res, next);
+};
+
+export const authRolesMiddleware = (roles) => (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const { role: userRole } = req.user;
+  if (!roles.includes(userRole)) {
+    return res.status(403).json({
+      message: "forbidden 😨",
+      return: "http://localhost:8080/views/profile",
+    });
+  }
+  next();
+};

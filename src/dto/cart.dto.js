6413e0e1012ed97f883ef6ac5acc35d92380bb52
georgeif2212 <jinfante2212@gmail.com>
2024-03-01 17:22:55 -0600

    fix: dto adapted to work with previous structure

--- src/dto/cart.dto.js
+++ src/dto/cart.dto.js
@@ -1,9 +1,11 @@
 import ProductDTO from "./product.dto.js";
+
 export default class CartDTO {
   constructor(cart) {
     this._id = cart._id || cart.id;
-    this.products = cart.products.map(
-      (product) => new ProductDTO(product.product)
-    );
+    this.products = cart.products.map((product) => {
+      const productDTO = new ProductDTO(product.product);
+      return { product: productDTO, quantity: product.quantity };
+    });
   }
 }

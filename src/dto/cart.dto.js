e4f80d21e43cd3c05459dcb4a2dfd2bad998723e
georgeif2212 <jinfante2212@gmail.com>
2024-03-01 15:56:16 -0600

    fix: dto applied to products from the car

--- src/dto/cart.dto.js
+++ src/dto/cart.dto.js
@@ -1,6 +1,9 @@
+import ProductDTO from "./product.dto.js";
 export default class CartDTO {
   constructor(cart) {
     this._id = cart._id || cart.id;
-    this.products = cart.products;
+    this.products = cart.products.map(
+      (product) => new ProductDTO(product.product)
+    );
   }
 }

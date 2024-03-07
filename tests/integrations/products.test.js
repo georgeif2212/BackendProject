f819f481699285f61fc61cce1f208a9095c05342
georgeif2212 <jinfante2212@gmail.com>
2024-03-06 23:27:57 -0600

    feat: test to create a new product

--- tests/integrations/products.test.js
+++ tests/integrations/products.test.js
@@ -5,11 +5,33 @@ const requester = supertest("http://localhost:8080");
 
 describe("Testing products router", function () {
   it("GET: Should get list of products", async function () {
-    const { statusCode, ok, _body } = await requester
-        .get('/api/products')
-    expect(statusCode).to.be.equal(200)
+    const { statusCode, ok, _body } = await requester.get("/api/products");
+    expect(statusCode).to.be.equal(200);
     expect(ok).to.be.ok;
-    expect(_body).to.be.has.property('status', 'success');
-    expect(_body).to.be.has.property('payload');
+    expect(_body).to.be.has.property("status", "success");
+    expect(_body).to.be.has.property("payload");
+  });
+
+  it("POST: Should create a new product", async function () {
+    const productMock = {
+      title: "Microondas Panasonic",
+      description:
+        "Microondas con capacidad de 1.2 cu. ft. y múltiples ajustes de cocción para una fácil preparación de alimentos.",
+      price: 1599,
+      thumbnail:
+        "https://www.costco.com.mx/medias/sys_master/products/h47/h1d/11023491596318.jpg",
+      code: `PAN${Date.now() / 1000}`,
+      stock: 11,
+      category: "cocina",
+      owner: "653a45ff59dcb07a93afk68b",
+    };
+
+    const { statusCode, ok, _body } = await requester
+      .post("/api/products")
+      .send(productMock);
+    expect(statusCode).to.be.equal(201);
+    expect(ok).to.be.ok;
+    expect(_body).to.be.has.property("_id");
+    expect(_body.owner).to.be.has.property("first_name");
   });
 });

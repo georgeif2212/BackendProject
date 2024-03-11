af712f8ea87e83cbcfa5ac0c91196b9e12dd2bd5
georgeif2212 <jinfante2212@gmail.com>
2024-03-11 14:05:08 -0600

    feat: new users endpoint to delete a user

--- src/routers/api/users.router.js
+++ src/routers/api/users.router.js
@@ -13,11 +13,15 @@ const router = Router();
 router.patch("/premium/:uid", async (req, res, next) => {
   try {
     await UsersController.premiumOrNotUser(req.params);
-    res.status(201).json({message:"The user role has been changed"})
+    res.status(201).json({ message: "The user role has been changed" });
   } catch (error) {
     next(error);
   }
 });
 
+router.delete("/:uid", async (req, res, next) => {
+  await UsersController.deleteById(req.params.uid);
+  res.status(204);
+});
 
 export default router;

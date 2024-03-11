a39c84b3d33036482fda4c105354423933713074
georgeif2212 <jinfante2212@gmail.com>
2024-03-11 14:06:32 -0600

    fix: unused imports weew removed

--- src/routers/api/users.router.js
+++ src/routers/api/users.router.js
@@ -1,12 +1,5 @@
 import { Router } from "express";
 import UsersController from "../../controllers/users.controller.js";
-import {
-  authMiddleware,
-  authRolesMiddleware,
-} from "../../middlewares/auth.middleware.js";
-import { CustomError } from "../../utils/CustomError.js";
-import { generatorProductIdError } from "../../utils/CauseMessageError.js";
-import EnumsError from "../../utils/EnumsError.js";
 
 const router = Router();
 

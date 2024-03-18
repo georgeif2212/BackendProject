a51841acd827d3702b650da28aa34a95b86fd943
georgeif2212 <jinfante2212@gmail.com>
2024-03-18 09:57:11 -0600

    feat: uploader modified to accept typefile as parameter function

--- src/utils/uploader.js
+++ src/utils/uploader.js
@@ -1,62 +1,70 @@
 import multer from "multer";
 import { __dirname } from "./utils.js";
+import path from "path";
+import fs from "fs";
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const {
-      params: { typeFile },
-    } = req;
-    let folderPath = null;
-    switch (typeFile) {
-      case "avatar":
-        folderPath = path.resolve(
-          __dirname,
-          "..",
-          "..",
-          "public",
-          "images",
-          "profiles"
-        );
-        break;
-      case "product":
-        folderPath = path.resolve(
-          __dirname,
-          "..",
-          "..",
-          "public",
-          "images",
-          "products"
-        );
-        break;
-      case "document":
-        folderPath = path.resolve(__dirname, "..", "..", "public", "documents");
-        break;
-      default:
-        return callback(new InvalidDataException("Ivalid type file 😱"));
-    }
-    fs.mkdirSync(folderPath, { recursive: true });
-    callback(null, folderPath);
-  },
-  filename: (req, file, callback) => {
-    const {
-      user: { _id },
-    } = req;
-    let filename = null;
-    switch (file.fieldname) {
-      case "identification":
-        filename = `${_id}_identification`;
-        break;
-      case "proofOfAddress":
-        filename = `${_id}_proofOfAddress`;
-        break;
-      case "bankStatement":
-        filename = `${_id}_bankStatement`;
-        break;
-      default:
-        filename = `${_id}_${file.originalname}`;
-    }
-    callback(null, filename);
-  },
-});
+export const uploaderMiddleware = (typeFile) => {
+  const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      let folderPath = null;
+      switch (typeFile) {
+        case "avatar":
+          folderPath = path.resolve(
+            __dirname,
+            "..",
+            "..",
+            "public",
+            "images",
+            "profiles"
+          );
+          break;
+        case "product":
+          folderPath = path.resolve(
+            __dirname,
+            "..",
+            "..",
+            "public",
+            "images",
+            "products"
+          );
+          break;
+        case "document":
+          folderPath = path.resolve(
+            __dirname,
+            "..",
+            "..",
+            "public",
+            "documents"
+          );
+          break;
+        default:
+          return callback(new InvalidDataException("Invalid type file 😱"));
+      }
+      fs.mkdirSync(folderPath, { recursive: true });
+      callback(null, folderPath);
+    },
+    filename: (req, file, callback) => {
+      const {
+        user: { _id },
+      } = req;
+      let filename = null;
+      switch (file.fieldname) {
+        case "identification":
+          filename = `${_id}_identification`;
+          break;
+        case "proofOfAddress":
+          filename = `${_id}_proofOfAdress`;
+          break;
+        case "bankStatement":
+          filename = `${_id}_bankStatement`;
+          break;
+        default:
+          filename = `${_id}_${file.originalname}`;
+      }
+      callback(null, filename);
+    },
+  });
 
-export const uploader = multer({ storage });
+  // Devuelve el middleware de Multer configurado
+  return multer({ storage });
+};

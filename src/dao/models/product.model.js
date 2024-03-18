877b369b18d15c578896409a126baebe554542d6
georgeif2212 <jinfante2212@gmail.com>
2024-03-18 15:14:10 -0600

    feat: product model adapted to work with multer files

--- src/dao/models/product.model.js
+++ src/dao/models/product.model.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
+const photoProductSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    reference: { type: String },
+  },
+  { _id: false }
+);
+
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    thumbnail: { type: String, required: true },
+    photos: { type: [photoProductSchema] },
     code: { type: String, required: true, unique: true },
     stock: { type: Number, required: true },
     category: { type: String, required: true },

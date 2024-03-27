4ae86eaa107fc84ec740ef9874cfdba183fa22ea
georgeif2212 <jinfante2212@gmail.com>
2024-03-26 22:20:40 -0600

    fix: stripe key iso now imported correctly

--- src/services/payments.service.js
+++ src/services/payments.service.js
@@ -1,8 +1,9 @@
 import Stripe from "stripe";
+import config from "../config/config";
 
 export default class PaymentsService {
   constructor() {
-    this.stripe = new Stripe("Secrete Key");
+    this.stripe = new Stripe(config.stripeBackendKey);
   }
   createPaymentIntent(data) {
     return this.stripe.paymentIntents.create(data);

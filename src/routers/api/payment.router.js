73eea105de54e60af39984aaefbebe1d2727a336
georgeif2212 <jinfante2212@gmail.com>
2024-04-04 17:51:03 -0600

    feat: main structure implemented

--- src/routers/api/payment.router.js
+++ src/routers/api/payment.router.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import {
+  authMiddleware,
+  authRolesMiddleware,
+} from "../../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/intent");
+router.get(
+  "/intent",
+  authRolesMiddleware[("user", "premium")],
+  authMiddleware("jwt"),
+  async (req, res, next) => {
+    
+
+  }
+);
 
 export default router;

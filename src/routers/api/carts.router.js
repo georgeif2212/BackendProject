3589e21cfb186819987798c7806690691cba9ee2
georgeif2212 <jinfante2212@gmail.com>
2024-04-06 19:17:44 -0600

    fix: payments service was removed but the endpoint to create a ticket is incomplete

--- src/routers/api/carts.router.js
+++ src/routers/api/carts.router.js
@@ -9,6 +9,7 @@ import TicketsController from "../../controllers/tickets.controller.js";
 import { CustomError } from "../../utils/CustomError.js";
 import { generatorProductIdError } from "../../utils/CauseMessageError.js";
 import EnumsError from "../../utils/EnumsError.js";
+import PaymentsService from "../../services/payments.service.js";
 
 const router = Router();
 
@@ -221,11 +222,9 @@ router.post(
   async (req, res, next) => {
     try {
       const { cartId } = req.params;
-      const availableProducts = await CartsController.doPurchase(cartId);
-      req.logger.debug("Debug message: availableProducts", availableProducts);
       const ticket = await TicketsController.create({
         availableProducts,
-        ...req.user,
+        user: req.user,
       });
       res.status(200).json(ticket);
     } catch (error) {

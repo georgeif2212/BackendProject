403ab9206a331e1d55a2a236650b8a54e04d6144
georgeif2212 <jinfante2212@gmail.com>
2024-04-07 17:33:13 -0600

    feat: new EMAIL template when the user generate a ticket and finish the purchase

--- src/utils/emailTemplates.js
+++ src/utils/emailTemplates.js
@@ -50,3 +50,66 @@ export const sendDeletedProductEmail = async (user, product) => {
   );
   return result;
 };
+
+export const sendSuccessfulPurchaseEmail = async (data) => {
+  const emailService = EmailService.getInstance();
+  const user = data.user;
+  const ticket = data.ticket;
+  const availableProducts = data.availableProducts;
+
+  const productsTableRows = availableProducts
+    .map((element) => {
+      return `
+      <tr>
+        <td>${element.product.title}</td>
+        <td>${element.quantity}</td>
+        <td>$${element.product.price}</td>
+      </tr>
+    `;
+    })
+    .join("");
+
+  const emailContent = `
+    <div>
+      <h1>Hola ${user.first_name}!</h1>
+      <p>Te notificamos que tu compra se ha completado con éxito.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Código de compra</th>
+            <th>Precio final</th>
+            <th>Email asociado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>${ticket.code}</td>
+            <td>$${ticket.amount}</td>
+            <td>${ticket.purchaser}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2>Productos comprados:</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Producto</th>
+            <th>Cantidad</th>
+            <th>Precio unitario</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${productsTableRows}
+        </tbody>
+      </table>
+    </div>
+  `;
+
+  const result = await emailService.sendEmail(
+    user.email,
+    `Tu compra ha sido exitosa`,
+    emailContent
+  );
+
+  return result;
+};

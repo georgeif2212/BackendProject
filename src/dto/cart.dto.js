ee7ec4f623b6aa42175e69b2164a31cdd9841f5b
georgeif2212 <jinfante2212@gmail.com>
2024-04-07 16:01:38 -0600

    fix: the cart dto must initialize the products as an array emtpy if there are no products

--- src/dto/cart.dto.js
+++ src/dto/cart.dto.js
@@ -3,9 +3,10 @@ import ProductDTO from "./product.dto.js";
 export default class CartDTO {
   constructor(cart) {
     this._id = cart._id || cart.id;
-    this.products = cart.products.map((product) => {
-      const productDTO = new ProductDTO(product.product);
-      return { product: productDTO, quantity: product.quantity };
-    });
+    this.products =
+      cart.products.map((product) => {
+        const productDTO = new ProductDTO(product.product);
+        return { product: productDTO, quantity: product.quantity };
+      }) || [];
   }
 }
